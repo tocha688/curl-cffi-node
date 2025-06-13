134ef78d94d960cd844d9ad5a7904203d64e55a4
tocha688 <tocha688@outlook.com>
2025-06-14 02:02:05 +0800

    防止全局接口重复加载

--- src/request/global.ts
+++ src/request/global.ts
@@ -2,7 +2,8 @@ import { curlGlobalInit, CurlMultiEvent } from "../impl";
 import { CurlClient } from "./request";
 
 curlGlobalInit();
-export const gimpl = new CurlMultiEvent();
+//@ts-ignore
+export const gimpl = global.gimpl = global.gimpl ?? new CurlMultiEvent();
 export const req = new CurlClient({ impl: gimpl });
 
 process.on("exit", () => {
