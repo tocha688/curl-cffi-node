785bb66ab50f042f9e1270530da5f14a5b202adf
tocha688 <tocha688@outlook.com>
2025-06-29 21:07:10 +0800

    test

--- tests/require.mjs
+++
@@ -1,6 +0,0 @@
-import { createRequire } from "node:module"
-import { getVersion, setLibPath } from '../dist/index.mjs'
-// const require = createRequire(import.meta.url)
-// const { getVersion } = require('..')
-setLibPath(process.cwd() + `/libs/x86_64-win32/bin/libcurl.dll`)
-console.log(getVersion())
