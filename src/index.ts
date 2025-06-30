91b099276c0fdcf81c0093f87465ffdedd275e6d
tocha688 <tocha688@outlook.com>
2025-07-01 00:53:49 +0800

    v0.1.21

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from "@tocha688/libcurl";
 export * from "./type";
 export * from "./request";
 export * from "./logger";
-export const getLibVersion = () => getVersion();
-export const getLibPath = () => getLibPathBase();
+export const libVersion = () => getVersion();
+export const libPath = () => getLibPathBase();
 
 
