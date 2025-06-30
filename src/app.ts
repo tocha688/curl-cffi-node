7941151104a8980d7ab6b0fe76b4284efbea9a6a
tocha688 <tocha688@outlook.com>
2025-06-30 23:20:29 +0800

    v0.1.19

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import os from 'node:os';
-
+import fs from 'node:fs';
 
 export const libsPath = path.join(__dirname, "..", 'libs');
 export const certPath = path.join(libsPath, "cacert.pem");
@@ -28,12 +28,18 @@ export function getDirName() {
 
 export function getLibPath() {
     const name = getDirName();
-    const libs: Record<string, string> = {
-        "win32": "bin/libcurl.dll",
-        // "darwin": "libcurl-impersonate.dylib",
-        "darwin": "libcurl-impersonate.4.dylib",
-        "linux": "libcurl-impersonate.so",
+    const libs: Record<string, string[]> = {
+        "win32": ["bin/libcurl.dll"],
+        "darwin": ["libcurl-impersonate.4.dylib", "libcurl-impersonate.dylib"],
+        "linux": ["libcurl-impersonate.so"],
     }
-    libs[os.arch()]
-    return path.join(libsPath, name, libs[os.platform()] || "libcurl-impersonate.so");
+    for (let lib of libs[os.platform()] || []) {
+        const libpath = path.join(libsPath, name, lib);
+        if (fs.existsSync(libpath)) {
+            return libpath;
+        }
+    }
+    // console.error(`libcurl not found for platform ${os.platform()} and arch ${os.arch()}`);
+    console.error(`Expected path: ${path.join(libsPath, name)}`);
+    throw new Error(`libcurl not found for platform ${os.platform()} and arch ${os.arch()}`);
 }
