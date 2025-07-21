06ec83a2816cf8bf5c231a3e1fa5ac5cea774086
tocha688 <tocha688@outlook.com>
2025-07-22 03:39:47 +0800

    优化退出

--- src/request/global.ts
+++ src/request/global.ts
@@ -6,6 +6,14 @@ curlGlobalInit();
 export const gimpl: CurlMultiEvent = global.gimpl = global.gimpl ?? new CurlMultiEvent();
 export const req = new CurlClient({ impl: gimpl });
 
-process.on("exit", () => {
+// 监听多个退出事件
+const cleanup = () => {
     gimpl.close();
-});
+};
+
+process.on("exit", cleanup);
+process.on("beforeExit", cleanup);
+process.on("SIGINT", cleanup);
+process.on("SIGTERM", cleanup);
+process.on("uncaughtException", cleanup);
+process.on("unhandledRejection", cleanup);
