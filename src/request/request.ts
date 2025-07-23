789d23a347747a13f4f1d7f0e08ace0fc8a512c8
tocha688 <tocha688@outlook.com>
2025-07-24 04:23:36 +0800

    如果接口以及关闭就重新打开一个

--- src/request/request.ts
+++ src/request/request.ts
@@ -1,4 +1,4 @@
-import {  CurlMultiImpl, requestSync } from "../impl";
+import { CurlMultiImpl, requestSync } from "../impl";
 import { CurlResponse, RequestOptions } from "../type";
 import { CurlRequestBase } from "./request_base";
 
@@ -8,7 +8,10 @@ export class CurlClientSync extends CurlRequestBase { }
 //异步方法
 export class CurlClient extends CurlRequestBase {
     constructor(ops?: RequestOptions) {
-        const impl = ops?.impl ?? (global as any)?.gimpl ?? new CurlMultiImpl();
+        let impl: CurlMultiImpl = ops?.impl ?? (global as any)?.gimpl ?? new CurlMultiImpl();
+        if (impl.closed) {
+            impl = new CurlMultiImpl();
+        }
         super({
             ...ops,
             impl
