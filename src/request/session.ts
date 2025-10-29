c96c90c7ee28a322c2e0f9f6b2e9b04390d4c864
tocha688 <tocha688@outlook.com>
2025-10-29 11:00:58 +0800

    优化session初始化参数

--- src/request/session.ts
+++ src/request/session.ts
@@ -1,17 +1,18 @@
 import { CookieJar } from "tough-cookie";
 import { CurlResponse, RequestOptions } from "../type";
-import { storageCurls } from "..";
+import { RequestInitOptions, storageCurls } from "..";
 import { CurlRequest } from "./CurlRequest";
+import { CurlPoolOptions } from "../core/CurlPool";
 
 /**
  * 会话客户端：在 baseOptions 中注入 CookieJar，并复用 CurlPool。
  */
 export class CurlSession extends CurlRequest {
-    constructor(ops?: RequestOptions) {
+    constructor(ops?: RequestInitOptions, poolOptions?: CurlPoolOptions) {
         super({
             ...ops,
             jar: ops?.jar ?? new CookieJar(),
-        });
+        }, poolOptions);
         storageCurls.add(this);
     }
 
